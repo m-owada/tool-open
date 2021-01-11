08f300673d3b19b3ac577da32aa758be855574e7
m-owada <m.owada.0629@live.jp>
2021-01-11 16:22:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# OPEN
+
+## 概要
+
+名前を入力してファイルを開くツールです。
+
+## 使い方
+
+- テキストボックスにファイル名を入力してEnterキーを押すと、指定したアプリケーションでファイルを開きます。
+- チェックボックスをONにすることで、常に手前に表示することが可能です。
+- 存在しないファイル名を入力した場合、前方一致するファイル名の一覧をリストボックスに表示します。
+- ファイルをリストボックスから選択して、ダブルクリック又はEnterキーを押すことで開くことが可能です。
+
+### 【コンパイル方法】
+
+- 「open.cs」と同じ場所に「compile.bat」を保存して実行してください。
+
+## 設定
+
+- ツールを起動すると設定ファイル「config.xml」が生成されます。必要に応じて設定内容を編集してください。
+
+### 【設定ファイル】
+
+```
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<config>
+  <x>10</x>
+  <y>10</y>
+  <width>220</width>
+  <height>160</height>
+  <topmost>True</topmost>
+  <path>C:\Users\Default\Documents</path>
+  <editor>C:\Windows\notepad.exe</editor>
+  <extension>txt</extension>
+  <lookahead>False</lookahead>
+</config>
+```
+
+### 【設定内容】
+
+|タグ     |説明                                                                |
+|---------|--------------------------------------------------------------------|
+|x        |ウィンドウの位置（X座標）                                           |
+|y        |ウィンドウの位置（Y座標）                                           |
+|width    |ウィンドウのサイズ（幅）                                            |
+|height   |ウィンドウのサイズ（高さ）                                          |
+|topmost  |常に手前に表示するか（True / False）                                |
+|path     |参照するフォルダパスを指定（複数可）                                |
+|editor   |ファイルを開くアプリケーションを指定（※）                          |
+|extension|対象とする拡張子を指定（省略可）                                    |
+|lookahead|アプリケーション起動時にファイルの一覧を先読みするか（True / False）|
+
+（※）特定の拡張子のファイルのみ別のアプリケーションを指定したい場合は、editorタグのtarget属性に対象となる拡張子を指定します。
+```
+<!-- 拡張子bmpのファイルの場合のみ使用するアプリケーションを指定 -->
+<editor target="bmp">C:\Windows\System32\mspaint.exe</editor>
+<!-- 上記以外の拡張子のファイルを開く場合に使用するアプリケーションを指定 -->
+<editor>C:\Windows\notepad.exe</editor>
+```
---
+++ open.cs
@@ -0,0 +1,471 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("OPEN")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright (c) 2020 m-owada.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        try
+        {
+            Application.EnableVisualStyles();
+            Application.ThreadException += (object sender, ThreadExceptionEventArgs e) =>
+            {
+                throw new Exception(e.Exception.Message);
+            };
+            Application.Run(new MainForm());
+        }
+        catch(Exception e)
+        {
+            MessageBox.Show(e.Message, e.Source);
+            Application.Exit();
+        }
+    }
+}
+
+class MainForm : Form
+{
+    private ComboBox comboBox = new ComboBox();
+    private CheckBox checkBox = new CheckBox();
+    private ListBox listBox = new ListBox();
+    private Dictionary<string, string> lookaheadFiles = new Dictionary<string, string>();
+    
+    public MainForm()
+    {
+        // フォーム
+        this.Text = "OPEN";
+        this.Size = new Size(220, 160);
+        this.MinimumSize  = this.Size;
+        this.Location = new Point(10, 10);
+        this.StartPosition = FormStartPosition.Manual;
+        this.FormBorderStyle = FormBorderStyle.Sizable;
+        this.FormClosing += OnFormClosing;
+        
+        // コンボボックス
+        comboBox.KeyDown += KeyDownComboBox;
+        comboBox.KeyPress += KeyPressComboBox;
+        comboBox.Text = string.Empty;
+        comboBox.Location = new Point(10, 10);
+        comboBox.Size = new Size(185, 20);
+        comboBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        SetComboBoxItems();
+        this.Controls.Add(comboBox);
+        
+        // チェックボックス
+        checkBox.CheckedChanged += ChangedCheckBox;
+        checkBox.Text = "常に手前に表示する";
+        checkBox.Location = new Point(10, 35);
+        checkBox.Size = new Size(125, 20);
+        checkBox.Checked = true;
+        checkBox.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+        this.Controls.Add(checkBox);
+        
+        // リストボックス
+        listBox.DoubleClick += DoubleClickListBox;
+        listBox.KeyDown += KeyDownListBox;
+        listBox.KeyPress += KeyPressListBox;
+        listBox.Leave += LeaveListBox;
+        listBox.Location = new Point(10, 60);
+        listBox.Size = new Size(185, 52);
+        listBox.MultiColumn = false;
+        listBox.SelectionMode = SelectionMode.MultiExtended;
+        listBox.IntegralHeight = false;
+        listBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        this.Controls.Add(listBox);
+        
+        LoadConfigSetting();
+    }
+    
+    private void OnFormClosing(object sender, FormClosingEventArgs e)
+    {
+        if(this.WindowState == FormWindowState.Normal)
+        {
+            var xml = LoadXmlFile("config.xml");
+            var config = xml.Element("config");
+            SetElementValue(config, "x", this.Location.X.ToString());
+            SetElementValue(config, "y", this.Location.Y.ToString());
+            SetElementValue(config, "width", this.Width.ToString());
+            SetElementValue(config, "height", this.Height.ToString());
+            SetElementValue(config, "topmost", checkBox.Checked.ToString());
+            xml.Save("config.xml");
+        }
+    }
+    
+    private void SetElementValue(XElement node, string name, string val)
+    {
+        if(node.Element(name) == null)
+        {
+            node.Add(new XElement(name, val));
+        }
+        else
+        {
+            node.Element(name).Value = val;
+        }
+    }
+    
+    private async void KeyDownComboBox(object sender, KeyEventArgs e)
+    {
+        if(e.KeyCode == Keys.Enter)
+        {
+            comboBox.Text = comboBox.Text.Replace(@"/", "").Replace(@"\", "");
+            if(!String.IsNullOrWhiteSpace(comboBox.Text))
+            {
+                await EnterComboBox();
+            }
+        }
+    }
+    
+    private async Task EnterComboBox()
+    {
+        comboBox.Enabled = false;
+        listBox.DataSource = null;
+        
+        await Task.Run(() =>
+        {
+            var items = new Dictionary<string, string>();
+            if(lookaheadFiles.Count > 0)
+            {
+                items = GetLookaheadFiles(comboBox.Text);
+            }
+            else
+            {
+                items = GetEnumerateFiles(comboBox.Text);
+            }
+            if(items.Count > 0)
+            {
+                listBox.BeginUpdate();
+                listBox.DisplayMember = "Key";
+                listBox.ValueMember = "Value";
+                listBox.DataSource = new BindingSource(items.OrderBy(i => i.Key), null);
+                listBox.EndUpdate();
+                SaveHistory(comboBox.Text);
+                SetComboBoxItems();
+                comboBox.Text = string.Empty;
+            }
+        });
+        
+        comboBox.Enabled = true;
+        if(listBox.Items.Count == 0)
+        {
+            comboBox.SelectionStart = 0;
+            comboBox.SelectionLength = comboBox.Text.Length;
+            comboBox.Focus();
+        }
+        else if(listBox.Items.Count == 1)
+        {
+            OpenFiles();
+            comboBox.Focus();
+        }
+        else
+        {
+            listBox.Focus();
+        }
+    }
+    
+    private Dictionary<string, string> GetEnumerateFiles(string keyword)
+    {
+        var items = new Dictionary<string, string>();
+        var config = LoadXmlFile("config.xml").Element("config");
+        var extensions = new List<string>();
+        foreach(var extension in config.Elements("extension"))
+        {
+            if(!String.IsNullOrWhiteSpace(extension.Value))
+            {
+                extensions.Add(extension.Value);
+            }
+        }
+        foreach(var path in config.Elements("path"))
+        {
+            foreach(var file in EnumerateFilesAllDirectories(path.Value, keyword + "*", extensions))
+            {
+                var name = Path.GetFileName(file);
+                if(!items.ContainsKey(name))
+                {
+                    items[name] = file;
+                }
+            }
+        }
+        return items;
+    }
+    
+    private IEnumerable<string> EnumerateFilesAllDirectories(string path, string searchPattern, List<string> extensions)
+    {
+        var files = Enumerable.Empty<string>();
+        try
+        {
+            files = Directory.EnumerateFiles(path, searchPattern);
+            if(extensions.Count > 0)
+            {
+                files = files.Where(file => extensions.Any(extension => file.EndsWith("." + extension, true, null)));
+            }
+        }
+        catch(UnauthorizedAccessException)
+        {
+        }
+        catch(PathTooLongException)
+        {
+        }
+        try
+        {
+            foreach(var p in Directory.EnumerateDirectories(path))
+            {
+                files = files.Union(EnumerateFilesAllDirectories(p, searchPattern, extensions));
+            }
+        }
+        catch(UnauthorizedAccessException)
+        {
+        }
+        catch(PathTooLongException)
+        {
+        }
+        return files;
+    }
+    
+    private void SaveHistory(string word)
+    {
+        var xml = LoadXmlFile("config.xml");
+        var config = xml.Element("config");
+        foreach(var history in config.Elements("history"))
+        {
+            if(word == history.Value)
+            {
+                history.Remove();
+            }
+        }
+        if(config.Elements("history").Count() >= 10)
+        {
+            config.Elements("history").First().Remove();
+        }
+        config.Add(new XElement("history", word));
+        xml.Save("config.xml");
+    }
+    
+    private void KeyPressComboBox(object sender, KeyPressEventArgs e)
+    {
+        if(e.KeyChar == (char)Keys.Enter)
+        {
+            e.Handled = true;
+        }
+    }
+    
+    private void SetComboBoxItems()
+    {
+        var items = new List<string>();
+        var config = LoadXmlFile("config.xml").Element("config");
+        foreach(var history in config.Elements("history"))
+        {
+            items.Add(history.Value);
+        }
+        items.Add(string.Empty);
+        items.Reverse();
+        comboBox.DataSource = null;
+        comboBox.Items.Add(new object());
+        comboBox.Items.Clear();
+        if(items.Count > 0)
+        {
+            comboBox.DataSource = items.ToArray();
+        }
+    }
+    
+    private void ChangedCheckBox(object sender, EventArgs e)
+    {
+        this.TopMost = checkBox.Checked;
+    }
+    
+    private void DoubleClickListBox(object sender, EventArgs e)
+    {
+        OpenFiles();
+    }
+    
+    private void KeyDownListBox(object sender, KeyEventArgs e)
+    {
+        if(e.KeyCode == Keys.Enter)
+        {
+            OpenFiles();
+        }
+    }
+    
+    private void KeyPressListBox(object sender, KeyPressEventArgs e)
+    {
+        if(e.KeyChar == (char)Keys.Enter)
+        {
+            e.Handled = true;
+        }
+    }
+    
+    private void LeaveListBox(object sender, EventArgs e)
+    {
+        listBox.Update();
+    }
+    
+    private void OpenFiles()
+    {
+        foreach(var item in listBox.SelectedItems)
+        {
+            var file = (KeyValuePair<string, string>)item;
+            if(File.Exists(file.Value))
+            {
+                var config = LoadXmlFile("config.xml").Element("config");
+                var app = string.Empty;
+                foreach(var editor in config.Elements("editor"))
+                {
+                    var target = editor.Attribute("target") == null ? string.Empty : editor.Attribute("target").Value;
+                    if(String.IsNullOrWhiteSpace(target))
+                    {
+                        app = String.IsNullOrWhiteSpace(app) ? editor.Value : app;
+                    }
+                    else
+                    {
+                        if(file.Value.EndsWith("." + target, true, null))
+                        {
+                            app = editor.Value;
+                            break;
+                        }
+                    }
+                }
+                if(String.IsNullOrWhiteSpace(app))
+                {
+                    MessageBox.Show("ファイル \"" + file.Key + "\" を起動するアプリケーションが指定されていません。", this.Text);
+                }
+                else
+                {
+                    Process.Start(app, file.Value);
+                }
+            }
+            else
+            {
+                MessageBox.Show("ファイル \"" + file.Key + "\" が見つかりません。", this.Text);
+            }
+        }
+    }
+    
+    private void LoadConfigSetting()
+    {
+        var config = LoadXmlFile("config.xml").Element("config");
+        
+        var x = this.Location.X;
+        foreach(var item in config.Elements("x"))
+        {
+            Int32.TryParse(item.Value, out x);
+            break;
+        }
+        var y = this.Location.Y;
+        foreach(var item in config.Elements("y"))
+        {
+            Int32.TryParse(item.Value, out y);
+            break;
+        }
+        this.Location = new Point(x, y);
+        
+        var width = this.Width;
+        foreach(var item in config.Elements("width"))
+        {
+            Int32.TryParse(item.Value, out width);
+            break;
+        }
+        var height = this.Height;
+        foreach(var item in config.Elements("height"))
+        {
+            Int32.TryParse(item.Value, out height);
+            break;
+        }
+        this.Size = new Size(width, height);
+        
+        var topmost = checkBox.Checked;
+        foreach(var item in config.Elements("topmost"))
+        {
+            Boolean.TryParse(item.Value, out topmost);
+            break;
+        }
+        checkBox.Checked = topmost;
+        
+        var lookahead = false;
+        foreach(var item in config.Elements("lookahead"))
+        {
+            Boolean.TryParse(item.Value, out lookahead);
+            break;
+        }
+        if(lookahead) SetLookaheadFiles();
+    }
+    
+    private async void SetLookaheadFiles()
+    {
+        comboBox.Enabled = false;
+        await Task.Run(() =>
+        {
+            lookaheadFiles = GetEnumerateFiles(string.Empty);
+        });
+        comboBox.Enabled = true;
+        comboBox.Focus();
+    }
+    
+    private Dictionary<string, string> GetLookaheadFiles(string keyword)
+    {
+        var items = new Dictionary<string, string>();
+        foreach(var file in lookaheadFiles)
+        {
+            if(IsWildcardMatch(file.Key, keyword + "*"))
+            {
+                items.Add(file.Key, file.Value);
+            }
+        }
+        return items;
+    }
+    
+    private bool IsWildcardMatch(string target, string keyword)
+    {
+        var regexPattern = Regex.Escape(keyword);
+        regexPattern = regexPattern.Replace(@"\*", ".*");
+        regexPattern = regexPattern.Replace(@"\?", ".*");
+        regexPattern = "^" + regexPattern;
+        return new Regex(regexPattern, RegexOptions.IgnoreCase).IsMatch(target);
+    }
+    
+    private XDocument LoadXmlFile(string file)
+    {
+        if(File.Exists(file))
+        {
+            return XDocument.Load(file);
+        }
+        else
+        {
+            var xml = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("config",
+                    new XElement("x", this.Location.X.ToString()),
+                    new XElement("y", this.Location.Y.ToString()),
+                    new XElement("width", this.Width.ToString()),
+                    new XElement("height", this.Height.ToString()),
+                    new XElement("topmost", "True"),
+                    new XElement("path", Environment.GetFolderPath(Environment.SpecialFolder.Personal)),
+                    new XElement("editor", @"C:\Windows\notepad.exe"),
+                    new XElement("extension", "txt"),
+                    new XElement("lookahead", "False")
+                )
+            );
+            xml.Save(file);
+            return xml;
+        }
+    }
+}
